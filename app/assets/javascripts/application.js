dc04eb3c60998ad62189de3c1ddb3c11f25de435
Sebastian Staudt <koraktor@gmail.com>
2013-04-12 07:13:58 +0200

    Optimize jQuery code a bit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,16 +8,18 @@ $(function() {
 });
 
 var formHandler = function() {
-  if($('#search').val() == $('#search')[0].defaultValue) {
-    $('#search').addClass('inactive');
+  var search = $('#search');
+
+  if(search.val() == search[0].defaultValue) {
+    search.addClass('inactive');
   }
 
-  $('#search').on('focus', function() {
+  search.on('focus', function() {
     $(this).removeClass('inactive');
     this.value = '';
   });
 
-  $('#search').blur(function() {
+  search.blur(function() {
     if($(this).val().match(/^\s*$/)) {
       $(this).addClass('inactive');
       this.value = this.defaultValue;
@@ -25,8 +27,8 @@ var formHandler = function() {
   });
 
   $('#search-form').submit(function() {
-      if(!$('#search').hasClass('inactive')) {
-          var searchUrl = '/search/' + $('#search').val();
+      if(!search.hasClass('inactive')) {
+          var searchUrl = '/search/' + search.val();
           if(typeof(repositoryName) !== 'undefined') {
             searchUrl = '/repos/' + repositoryName + searchUrl;
           }
