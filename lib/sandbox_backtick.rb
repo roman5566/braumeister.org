b29280007a2c2c9e3183d09e5a31852effdb78e8
Sebastian Staudt <koraktor@gmail.com>
2013-12-15 16:40:06 +0100

    Update and improve backtick sandbox

--- lib/sandbox_backtick.rb
+++ lib/sandbox_backtick.rb
@@ -1,7 +1,7 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2012, Sebastian Staudt
+# Copyright (c) 2012-2013, Sebastian Staudt
 
 module Kernel
 
@@ -12,10 +12,10 @@ module Kernel
       File.join $homebrew_path, 'bin', 'brew'
     elsif command == '/usr/bin/mdfind "kMDItemCFBundleIdentifier == \'com.apple.dt.Xcode\'"'
       '/Applications/Xcode.app'
-    elsif command == '/usr/bin/sw_vers -productVersion'
-      '10.8'
+    elsif command =~ /sw_vers -productVersion$/
+      '10.9'
     elsif command == 'xcodebuild -version 2>&1'
-      "Xcode 4.4\nBuild version 4F250"
+      "Xcode 5.0.2\nBuild version 5A3005"
     elsif command == 'php -v'
       "5.4"
     else
