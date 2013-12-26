b58ce0de9f1c4a62a2c33fcdb75172d538664801
Sebastian Staudt <koraktor@gmail.com>
2013-12-26 12:03:37 +0100

    Remove handling and style for inactive search field

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,36 +4,14 @@
 
 $(function() {
   $('abbr.timeago').timeago();
-});
-
-var formHandler = function() {
-  var search = $('#search');
-
-  if(search.val() == search[0].defaultValue) {
-    search.addClass('inactive');
-  }
-
-  search.on('focus', function() {
-    $(this).removeClass('inactive');
-    this.value = '';
-  });
-
-  search.blur(function() {
-    if($(this).val().match(/^\s*$/)) {
-      $(this).addClass('inactive');
-      this.value = this.defaultValue;
-    }
-  });
 
   $('#search-form').submit(function() {
-      if(!search.hasClass('inactive')) {
-          var searchUrl = '/search/' + search.val();
-          if(typeof(repositoryName) !== 'undefined') {
-            searchUrl = '/repos/' + repositoryName + searchUrl;
-          }
-          window.location = searchUrl;
-      }
+    var searchUrl = '/search/' + $('#search').val();
+    if(typeof(repositoryName) !== 'undefined') {
+      searchUrl = '/repos/' + repositoryName + searchUrl;
+    }
+    window.location = searchUrl;
 
-      return false;
+    return false;
   });
-}
+});
