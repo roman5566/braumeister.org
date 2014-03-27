5e66788b55aeb37aa7485ca4c334edeebc1f8182
Sebastian Staudt <koraktor@gmail.com>
2014-03-27 20:41:40 +0100

    Test all of Formula’s methods

--- spec/models/formula_spec.rb
+++ spec/models/formula_spec.rb
@@ -1,18 +1,69 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2012, Sebastian Staudt
+# Copyright (c) 2012-2014, Sebastian Staudt
 
 require 'spec_helper'
 
 describe Formula do
 
-  context 'for a formula in a full repository' do
+  let :formula do
+    repo = Repository.new name: Repository::MAIN, full: true
+    Formula.new name: 'git', repository: repo
+  end
 
-    let(:formula) do
-      repo = Repository.new name: Repository::MAIN, full: true
-      Formula.new name: 'git', repository: repo
+  describe '#set_id' do
+    it 'should update the formula’s id' do
+      formula.send :set_id
+
+      formula.id.should eq "#{Repository::MAIN}/git"
     end
+  end
+
+  describe '#update_metadata' do
+    it 'updates the metadata of the formula' do
+      formula_info = {
+        homepage: 'http://example.com',
+        keg_only: true,
+        stable_version: '1.0.0',
+        devel_version:  '1.1.0.beta',
+        head_version: 'HEAD'
+      }
+
+      formula.update_metadata formula_info
+
+      formula.homepage.should eq 'http://example.com'
+      formula.keg_only.should be true
+      formula.stable_version.should eq '1.0.0'
+      formula.devel_version.should eq '1.1.0.beta'
+      formula.head_version.should eq 'HEAD'
+    end
+  end
+
+  describe '#version' do
+    it 'should return the stable version if it is available' do
+      formula.stable_version = '1.0.0'
+      formula.devel_version = '1.1.0.beta'
+      formula.head_version = 'HEAD'
+
+      formula.version.should eq '1.0.0'
+    end
+
+    it 'should return the devel version if it is available and no stable version exists' do
+      formula.devel_version = '1.1.0.beta'
+      formula.head_version = 'HEAD'
+
+      formula.version.should eq '1.1.0.beta'
+    end
+
+    it 'should return the head version if no other version exists' do
+      formula.head_version = 'HEAD'
+
+      formula.version.should eq 'HEAD'
+    end
+  end
+
+  context 'for a formula in a full repository' do
 
     describe '#path' do
       it 'returns the relative path' do
@@ -30,7 +81,7 @@ describe Formula do
 
   context 'for a formula in an alternative repository' do
 
-    let(:formula) do
+    let :formula do
       repo = Repository.new name: 'adamv/homebrew-alt', full: false
       Formula.new name: 'php', path: 'duplicates', repository: repo
     end
