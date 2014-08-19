2646fa351b5f148e10edde5a803fb90d98b71327
Sebastian Staudt <koraktor@gmail.com>
2014-08-19 22:18:06 +0200

    Add a fallback to `OS::Mac`
    Currently, Homebrew still provides `MacOS`, but it is already
    deprecated.

--- lib/sandbox_macos.rb
+++ lib/sandbox_macos.rb
@@ -1,9 +1,13 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2013, Sebastian Staudt
+# Copyright (c) 2013-2014, Sebastian Staudt
 
-if defined? MacOS
+unless defined? ::MacOS
+  ::MacOS = ::OS::Mac if defined? ::OS::Mac
+end
+
+if defined? ::MacOS
   MacOS.methods.select { |m| m.to_s =~ /_version$/ }.each do |m|
     MacOS.send :undef_method, m
     MacOS.send :define_method, m, ->{ '0' }
