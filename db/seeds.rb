93fc4d64da30086ba0f1145a19b1ffa9c2393d38
Sebastian Staudt <koraktor@gmail.com>
2014-08-20 07:29:19 +0200

    Add PHP and Python taps to DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2012-2013, Sebastian Staudt
+# Copyright (c) 2012-2014, Sebastian Staudt
 
 Repository.find_or_create_by name: 'Homebrew/homebrew'
 Repository.find_or_create_by name: 'Homebrew/homebrew-apache'
@@ -10,6 +10,8 @@ Repository.find_or_create_by name: 'Homebrew/homebrew-boneyard'
 Repository.find_or_create_by name: 'Homebrew/homebrew-completions'
 Repository.find_or_create_by name: 'Homebrew/homebrew-dupes'
 Repository.find_or_create_by name: 'Homebrew/homebrew-games'
+Repository.find_or_create_by name: 'Homebrew/homebrew-php'
+Repository.find_or_create_by name: 'Homebrew/homebrew-python'
 Repository.find_or_create_by name: 'Homebrew/homebrew-headonly'
 Repository.find_or_create_by name: 'Homebrew/homebrew-science'
 Repository.find_or_create_by name: 'Homebrew/homebrew-versions'
