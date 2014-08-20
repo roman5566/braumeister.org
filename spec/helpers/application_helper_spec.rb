decde679e05210ac56b4c585efb37b15010ddd44
Sebastian Staudt <koraktor@gmail.com>
2014-08-20 23:19:27 +0200

    Fix some problems in `ApplicationHelper` spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,15 +9,15 @@ describe ApplicationHelper do
 
   describe '#formulae_link' do
     it 'provides links to formulae in the main repository' do
-      repo = Repository.create name: Repository::MAIN
-      formula = repo.formulae.create name: 'git'
+      repo = Repository.find_or_create_by name: Repository::MAIN
+      formula = repo.formulae.find_or_create_by name: 'git'
 
       expect(helper.formula_link(formula)).to eq('<a class="formula" href="/formula/git">git</a>')
     end
 
     it 'provides links to formulae in a tap repository' do
-      repo = Repository.create name: 'Homebrew/homebrew-science'
-      formula = repo.formulae.create name: 'gromacs'
+      repo = Repository.find_or_create_by name: 'Homebrew/homebrew-science'
+      formula = repo.formulae.find_or_create_by name: 'gromacs'
 
       expect(helper.formula_link(formula)).to eq('<a class="formula" href="/repos/Homebrew/homebrew-science/formula/gromacs">gromacs</a>')
     end
@@ -25,7 +25,7 @@ describe ApplicationHelper do
 
   describe '#timestamp' do
     it 'provides a timestamp tag' do
-      time = Time.at 1397573100
+      time = ActiveSupport::TimeZone['Berlin'].at 1397573100
       expect(helper.timestamp(time)).to eq('<time class="timeago" datetime="2014-04-15T14:45:00Z">April 15, 2014 16:45</time>')
     end
   end
