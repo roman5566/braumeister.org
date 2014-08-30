roman5566/braumeister.org

commit 9146334d738305f047586ea3aa21c2e292ae8be3

Decouple Repository#refresh and #generate_history
This enables to regenerate repositories in the right order, i.e.
recovering deleted formulae before generating the history.

app/models/repository.rb
@@ -218,9 +218,9 @@ class Repository
       formula.save!
     end
 
-    generate_history last_sha
-
     Rails.logger.info "#{added} formulae added, #{modified} formulae modified, #{removed} formulae removed."
+
+    last_sha
   end
 
   def regenerate!
@@ -241,6 +241,7 @@ class Repository
 
     refresh
     recover_deleted_formulae
+    generate_history
   end
 
   def reset_head(sha = self.sha)

lib/tasks/braumeister.rake
@@ -61,7 +61,10 @@ namespace :braumeister do
   desc 'Pulls the latest changes from one or all repositories'
   task_with_tracing :update, [:repo] => :select_repos do
     @repos.each do |repo|
-      airbrake_rescued { repo.refresh }
+      airbrake_rescued do
+        last_sha = repo.refresh
+        repo.generate_history last_sha
+      end
     end
   end
 

commit 3303c210ee52d61198001f534def27ac95cc60a3

Fix accidentally regenerating whole history
This was caused by 9146334d738305f047586ea3aa21c2e292ae8be3 and would
regenerate repository history in each update where `HEAD` of the
repository did not change.

app/models/repository.rb
@@ -163,7 +163,7 @@ class Repository
       Rails.logger.info 'No formulae changed.'
       touch
       save!
-      return
+      return last_sha
     end
 
     formulae_info = formulae_info formulae.map { |type, fpath|