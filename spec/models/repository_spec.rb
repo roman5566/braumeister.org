7ec855f6fb4cbbeae41509927b772be30b0faa4a
Sebastian Staudt <koraktor@gmail.com>
2014-08-31 14:04:40 +0200

    Add some missing specs for Repository

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -19,6 +19,17 @@ describe Repository do
     end
   end
 
+  describe '#feed_link' do
+    it 'returns the short feed link for the main repositiory' do
+      expect(repo.feed_link).to eq('/feed.atom')
+    end
+
+    it 'returns the full feed link for other repositiories' do
+      repo.name = 'Homebrew/homebrew-games'
+      expect(repo.feed_link).to eq('/repos/Homebrew/homebrew-games/feed.atom')
+    end
+  end
+
   describe '#main?' do
     it "returns true for #{Repository::MAIN}" do
       expect(repo.main?).to be_truthy
@@ -26,6 +37,12 @@ describe Repository do
 
     it 'returns false for other repositories' do
       expect(Repository.new(name: 'adamv/homebrew-alt').main?).to be_falsey
+    end
+  end
+
+  describe '#to_param' do
+    it 'returns the name of the repository' do
+      expect(repo.to_param).to eq(Repository::MAIN)
     end
   end
 
