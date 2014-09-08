93a0d9618dcab4c1915105ad42c5cfed77afa798
Sebastian Staudt <koraktor@gmail.com>
2014-09-08 07:13:26 +0000

    Fix sandboxed Formulary to use RepositoryImport

--- lib/sandbox_formulary.rb
+++ lib/sandbox_formulary.rb
@@ -6,7 +6,10 @@ class Formulary
 
   def self.factory(ref)
     path = nil
-    repo = Repository.all.detect { |repo| path = repo.find_formula ref }
+    repo = Repository.all.detect do |repo|
+      repo.extend RepositoryImport
+      path = repo.find_formula ref
+    end
     original_factory(path.nil? ? ref : File.join(repo.path, path))
   end
 
